98e33f700d9dfe42d91dc96ef02add147b8ff3ec
jmika-coding <mika78500@hotmail.fr>
2022-11-04 19:04:50 +0100

    Add prettier formatting  on .cjs files + Use React new JSX transform

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,18 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "plugin:react/recommended"],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { tsconfigRootDir: __dirname, project: ["./tsconfig.json"], sourceType: "module", ecmaVersion: 2022 },
-  plugins: ['@typescript-eslint'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+    ecmaVersion: 2022,
+  },
+  plugins: ["@typescript-eslint"],
   env: { browser: true, node: true, es2022: true },
-  root: true
-};
+  root: true,
+}
