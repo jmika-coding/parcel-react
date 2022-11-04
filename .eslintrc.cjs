e98891c7d6f56afc88e057c77d696f4270972f4c
jmika-coding <mika78500@hotmail.fr>
2022-11-04 18:19:57 +0100

    Use ESM instead of CommonJS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
-/* eslint-disable */
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', "plugin:react/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: { tsconfigRootDir: __dirname, project: ["./tsconfig.json"], sourceType: "module", ecmaVersion: 2022 },
   plugins: ['@typescript-eslint'],
-  root: true,
+  env: { browser: true, node: true, es2022: true },
+  root: true
 };
